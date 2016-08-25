f5987a91e1c60d382339b7da0c2d4e0a3267e004
oljashabanova <sh.olja@gmail.com>
2016-08-25 16:34:18 +0300

    hardcode  path to chromedriver  ( current path  fails on Linux env )

--- framework/src/main/java/com/pipedrive/utils/WebDriverUtils.java
+++ framework/src/main/java/com/pipedrive/utils/WebDriverUtils.java
@@ -1,8 +1,5 @@
 package com.pipedrive.utils;
 
-import org.apache.commons.lang3.SystemUtils;
-import java.util.Optional;
-
 import static java.lang.ClassLoader.getSystemResource;
 
 
@@ -10,15 +7,16 @@ public final class WebDriverUtils {
 
     public static void setChromeDriverPath() {
 
-        final String driverName = "chromedriver_" +
+        /*final String driverName = "chromedriver_" +
                 (SystemUtils.IS_OS_WINDOWS ? "win32.exe" :
                         SystemUtils.IS_OS_MAC || SystemUtils.IS_OS_MAC_OSX ? "mac32" :
                                 Optional.ofNullable(SystemUtils.OS_ARCH)
                                         .filter(arch -> arch.contains("86"))
                                         .map(arch -> "linux32")
-                                        .orElse("linux64"));
+                                        .orElse("linux64"));*/
 
-        System.setProperty("webdriver.chrome.driver", getSystemResource("drivers/" + driverName).getFile());
+        //System.setProperty("webdriver.chrome.driver", getSystemResource("drivers/" + driverName).getFile());
+        System.setProperty("webdriver.chrome.driver", getSystemResource("drivers/chromedriver_linux64").getFile());
     }
 
     public static void setIEDriverPath(){
