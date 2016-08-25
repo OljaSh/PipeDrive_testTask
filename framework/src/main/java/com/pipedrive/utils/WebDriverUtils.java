6b9ecb857fe2b4b7c484f4628348a071566bc7ab
oljashabanova <sh.olja@gmail.com>
2016-08-25 20:09:21 +0300

    - uncomment  driverName

--- framework/src/main/java/com/pipedrive/utils/WebDriverUtils.java
+++ framework/src/main/java/com/pipedrive/utils/WebDriverUtils.java
@@ -1,5 +1,9 @@
 package com.pipedrive.utils;
 
+import org.apache.commons.lang.SystemUtils;
+
+import java.util.Optional;
+
 import static java.lang.ClassLoader.getSystemResource;
 
 
@@ -7,16 +11,16 @@ public final class WebDriverUtils {
 
     public static void setChromeDriverPath() {
 
-        /*final String driverName = "chromedriver_" +
+        final String driverName = "chromedriver_" +
                 (SystemUtils.IS_OS_WINDOWS ? "win32.exe" :
                         SystemUtils.IS_OS_MAC || SystemUtils.IS_OS_MAC_OSX ? "mac32" :
                                 Optional.ofNullable(SystemUtils.OS_ARCH)
                                         .filter(arch -> arch.contains("86"))
                                         .map(arch -> "linux32")
-                                        .orElse("linux64"));*/
+                                        .orElse("linux64"));
 
-        //System.setProperty("webdriver.chrome.driver", getSystemResource("drivers/" + driverName).getFile());
-        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver_linux64");
+        System.setProperty("webdriver.chrome.driver", getSystemResource("drivers/" + driverName).getFile());
+       // System.setProperty("webdriver.chrome.driver", "drivers/chromedriver_linux64");
     }
 
     public static void setIEDriverPath(){
