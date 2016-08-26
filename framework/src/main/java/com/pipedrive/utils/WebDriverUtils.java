91f693ac9b92662abef2b0fb47e068966384aa6e
oljashabanova <sh.olja@gmail.com>
2016-08-26 09:13:44 +0300

    - change chrome webdriver loading

--- framework/src/main/java/com/pipedrive/utils/WebDriverUtils.java
+++ framework/src/main/java/com/pipedrive/utils/WebDriverUtils.java
@@ -11,15 +11,15 @@ public final class WebDriverUtils {
 
     public static void setChromeDriverPath() {
 
-        final String driverName = "chromedriver_" +
+        final String driverName = "drivers/chromedriver_" +
                 (SystemUtils.IS_OS_WINDOWS ? "win32.exe" :
                         SystemUtils.IS_OS_MAC || SystemUtils.IS_OS_MAC_OSX ? "mac32" :
                                 Optional.ofNullable(SystemUtils.OS_ARCH)
                                         .filter(arch -> arch.contains("86"))
                                         .map(arch -> "linux32")
                                         .orElse("linux64"));
-
-        System.setProperty("webdriver.chrome.driver", getSystemResource("drivers/" + driverName).getPath());
+System.setProperty("webdriver.chrome.driver", getSystemResource(driverName).getPath());
+        // System.setProperty("webdriver.chrome.driver", getSystemResource("drivers/" + driverName).getFile());
        // System.setProperty("webdriver.chrome.driver", "drivers/chromedriver_linux64");
 
     }
